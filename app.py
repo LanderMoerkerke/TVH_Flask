4d92e4fc5de364bdb001e2eae131302888ba1575
jacob <jacob.verschaeve@student.howest.be>
2018-06-18 15:13:14 +0200

    Changed Paths (MAC)

--- app.py
+++ app.py
@@ -105,11 +105,12 @@ klasses = [
 ]
 
 # Initialise model
-filename_model = 'static/model/my_model_test.h5'
+filename_model = os.path.join(os.path.dirname(__file__), 'static/model', 'my_model_test.h5')
+
 model = load_model(filename_model)
 model._make_predict_function()
 dataset_one_hot_encoding = pd.read_excel(
-    open('static/data/one_hot_encoding.xlsx', 'rb'), sheet_name='Sheet1')
+    open(os.path.join(os.path.dirname(__file__), 'static/data', 'one_hot_encoding.xlsx'), 'rb'), sheet_name='Sheet1')
 
 
 @app.route('/')
@@ -120,7 +121,7 @@ def index():
 
 photos = UploadSet('photos', IMAGES)
 
-app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(os.path.dirname(__file__), 'static/uploads')
 configure_uploads(app, photos)
 
 
@@ -134,7 +135,7 @@ def upload():
         print(filename)
 
         # Inlezen van image (gekregen van vorige pagina, beslissen in load_image naar welke pagina.)
-        _IMAGE = 'static/uploads/%s' % filename
+        _IMAGE = os.path.join(os.path.dirname(__file__), 'static/uploads', filename)
         _IMAGE = imread(_IMAGE, as_grey=1)
         _IMAGE = _IMAGE.reshape(-1, 256, 256, 1)
 
@@ -213,26 +214,35 @@ def button_press_yes():
     print('counter answer', counter_answer)
     print('yes lijst', _QUESTIONS)
     if counter_answer == 1:
-        return render_template('questions.html', lijst=_QUESTIONS[1])
+        print("YES x1")
+        _PERC = d.get(int(_QUESTIONS[4]).__str__())
+        return prediction(_QUESTIONS[4], _PERC)
+        # return render_template('questions.html', lijst = _QUESTIONS[1])
     else:
-        _PERC = d.get(int(_QUESTIONS[2]).__str__())
-        return prediction(_QUESTIONS[2], _PERC)
+        print("YES als je NEE eerder antwoordde.")
+        _PERC = d.get(int(_QUESTIONS[3]).__str__())
+        return prediction(_QUESTIONS[3], _PERC)
         #return render_template('prediction.html', group=lijst[2], maximum=_PERC)
-
 
 @app.route('/button_press_no')
 def button_press_no():
-    global _QUESTIONS
+    global _QUESTIONS, counter_answer
     global d
-    print('no lijst', _QUESTIONS)
-    if counter_answer == 0:
-        _PERC = d.get(int(_QUESTIONS[4]).__str__())
-        return prediction(_QUESTIONS[4], _PERC)
+    counter_answer += 1
+    print('no lijst' ,_QUESTIONS)
+    if counter_answer == 1:
+        print("NEE 1x")
+        return render_template('questions.html', lijst = _QUESTIONS[1])
+
+        #_PERC = d.get(int(_QUESTIONS[4]).__str__())
+        #return prediction(_QUESTIONS[4], _PERC)
         #return render_template('prediction.html', group=lijst[4], maximum=_PERC)
     else:
-        _PERC = d.get(int(_QUESTIONS[3]).__str__())
-        return prediction(_QUESTIONS[3], _PERC)
+        print("NEE 2x")
+        _PERC = d.get(int(_QUESTIONS[2]).__str__())
+        return prediction(_QUESTIONS[2], _PERC)
         #return render_template('prediction.html', group=lijst[3], maximum=_PERC)
+
 
 
 @app.route('/prediction/<group>/<maximum>')
@@ -240,7 +250,7 @@ def prediction(group, maximum):
     print('prediction vars', group, maximum)
 
     d_chars = pd.read_excel(
-        open('static/data/output.xlsx', 'rb'), sheet_name='Sheet1')
+        open(os.path.join(os.path.dirname(__file__), 'static/data', 'output.xlsx'), 'rb'), sheet_name='Sheet1')
 
     chars_list = list(
         d_chars.loc[d_chars.GroupCode.isin([int(group)]), 'CHARS'])
